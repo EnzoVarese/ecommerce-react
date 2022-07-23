6a93279be3d0da0f1ba41843ab9889cf0dc5bc91
Enzo Varese <enzovarese1@gmail.com>
2022-07-23 17:40:24 -0300

    agregando caracteristica

--- src/index.js
+++ src/index.js
@@ -2,7 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//BOOTSTRAP
 import 'bootstrap/dist/css/bootstrap.min.css';
+//FIREBASE
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBmLF8ARHafaZYgry6TTjgOqfRHyTjJ_To",
+  authDomain: "beleza-imports.firebaseapp.com",
+  projectId: "beleza-imports",
+  storageBucket: "beleza-imports.appspot.com",
+  messagingSenderId: "602005686492",
+  appId: "1:602005686492:web:a085f8676c42ef3ac70c19"
+};
+
+
+const app = initializeApp(firebaseConfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
